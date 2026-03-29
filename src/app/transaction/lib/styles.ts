4de5a5f9f355f332253d9e2fe2ffdb50f1b04d1e
ins1pid <sierra.miguel03@gmail.com>
2026-03-29 16:30:26 -0700

    [style] Improved UI/UX of /transaction page
    --------------------------------------------------------------------------------
    Type:
    --------------------------------------------------------------------------------
    [style]    UI/design changes
    
    --------------------------------------------------------------------------------
    Short Description:
    --------------------------------------------------------------------------------
    Improved UI/UX of /transaction page
    
    --------------------------------------------------------------------------------
    Changes:
    --------------------------------------------------------------------------------
    - Formatted table towards middle
    - Changed button format
    - Added some mobile format improvements
    - Added dark/light mode support
    
    --------------------------------------------------------------------------------
    Reasoning:
    --------------------------------------------------------------------------------
    Ease-of-use
    
    --------------------------------------------------------------------------------
    Notes:
    --------------------------------------------------------------------------------
    Added a styles.ts file, but maybe we modify globals.css for colors we'll
    be using in our site.

---
+++ src/app/transaction/lib/styles.ts
@@ -0,0 +1,9 @@
+export const textColors = {
+  primary: "text-gray-900 dark:text-gray-100",
+  secondary: "text-gray-600 dark:text-gray-400",
+}
+
+export const bgColors = {
+  primary: "bg-white dark:bg-black",
+  secondary: "bg-gray-200 dark:bg-gray-800",
+}
